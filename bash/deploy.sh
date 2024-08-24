b7579dfba292b62b535706746d2c45e1dfd6c847
yusuke <theshoreunder@gmail.com>
2024-08-24 10:48:45 +0900

    Add github workflow

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,2 +1,10 @@
 docker build -t asia-northeast1-docker.pkg.dev/ksst-genai-app/genai-app/english-learning-feedback:latest .
 docker push asia-northeast1-docker.pkg.dev/ksst-genai-app/genai-app/english-learning-feedback:latest
+gcloud run deploy english-learning-feedback \
+  --image asia-northeast1-docker.pkg.dev/ksst-genai-app/genai-app/english-learning-feedback:latest \
+  --platform managed \
+  --region asia-northeast1 \
+  --allow-unauthenticated \
+  --max-instances 1 \
+  --min-instances 0 \
+  --memory 512Mi
